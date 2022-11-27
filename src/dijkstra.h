adc2baa956fecd016ae1c0f9dc18f5fa75fd0495
Dani <danbodker@gmail.com>
2022-11-27 16:54:06 +0100

    fixed files so the implementation is in the source file, instead of the header

--- src/dijkstra.h
+++ src/dijkstra.h
@@ -1,35 +1,9 @@
 #pragma once
 
-#include <stdio.h>
-#include <malloc.h>
+int* create_adjacency_matrix_for_dijkstra_algorithm(int number_of_stations);
 
-int* create_adjacency_matrix_for_dijkstra_algorithm(int number_of_stations)
-{
-    int (*adjacency_matrix)[number_of_stations] = malloc(sizeof(int[number_of_stations][number_of_stations]));
 
-    // fill matrix with zeros, so stations are disconnected
-    for(int j = 0; j < number_of_stations; j++) {
-        for (int i = 0; i < number_of_stations; i++) {
-            adjacency_matrix[i][j] = 0;
-        }
-    }
+void calculate_optimal_route();
 
-    return *adjacency_matrix;
 
-}
-
-void calculate_optimal_route()
-{
-
-}
-
-void print_matrix(int size, int*matrix)
-{
-    int i, j;
-    for(i=0; i<size; i++) {
-        for (j = 0; j < size; j++) {
-            printf("%d ", *(matrix + i * size + j)); // totally not copy-pasted code from stack overflow
-        }
-        printf("\n");
-    }
-}
+void print_matrix(int size, int*matrix);

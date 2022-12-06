843933e584bae0536f03368c70456e1ec888c992
AndreasBM1 <Ak42an@student.aau.dk>
2022-12-06 11:33:59 +0100

    Validating stations fixed

--- src/UI.c
+++ src/UI.c
@@ -8,78 +8,41 @@
 #include "station.h"
 #include <stdio.h>
 #include <malloc.h>
-/*
-char* input_from_user ()
-{
-    char user_input_start_station [100],
-         user_input_end_station [100];
+#include <stdbool.h>
 
-    printf("Where does your journey start from\n");
-    scanf("%s ", user_input_start_station);
+Station* start_station_from_user (Station* station){
+    char user_input_start_station;
+    bool station_is_valid = false;
 
+    while (station_is_valid == false){
+        printf("Where does your journey start from\n");
+        scanf("%s ", &user_input_start_station);
 
-    // check if the station is valid. If invalid ask for first station again
-    if (validate_input(user_input_start_station, Station station) == 1) {
-        printf("Where would you like to go?\n");
-        scanf("%s ", user_input_end_station);
-
-
-        // Check if destination station is valid. If invalid ask for destination station again
-        if (validate_input(user_input_end_station, Station station) == 0){
-            printf("invalid input\n");
-
-            // do something to get second station again that is not stupid
-        }
-
+        // checking if the station is in our list and returning a true if the station is present
+        station_is_valid = (bool)get_station_by_id(station, user_input_start_station);
     }
-    else {
-        printf("Invalid station\n");
-
-        // do something to get first station again that is not stupid
-    }
-}
-*/
-
-char* start_station_from_user (Station* station){
-    char* user_input_start_station = malloc(sizeof(char));
-    int station_is_valid = 0;
-
-    while (station_is_valid == 0){
-    printf("Where does your journey start from\n");
-    scanf("%s ", user_input_start_station);
-    station_is_valid = validate_input(user_input_start_station, Station* station);
-    }
-    return user_input_start_station;
+    
+    // returns the station by id
+    return get_station_by_id(station, user_input_start_station);
 }
 
-char* end_station_from_user (Station* station){
-    char* user_input_end_station = malloc (sizeof(char));
-    int station_is_valid = 0;
+Station* end_station_from_user (Station* station){
+    char user_input_end_station;
+    bool station_is_valid = false;
 
-    while (station_is_valid == 0){
+    while (station_is_valid == false){
         printf("Where does your journey end\n");
-        scanf("%s ", user_input_end_station);
-        station_is_valid = validate_input(user_input_end_station, Station* station);
+        scanf("%s ", &user_input_end_station);
+
+        // checking if the station is in our list and returning a true if the station is present
+        station_is_valid = (bool)get_station_by_id(station, user_input_end_station);
     }
-    return user_input_end_station;
+
+    // returns the station by id
+    return get_station_by_id(station, user_input_end_station);
 }
 
 
-// Function for checking whether the input station is valid or invalid
-int validate_input(int user_input_station, Station station){
-
-    int number_of_stations = sizeof(station.id);
-
-    for (int i = 0; i < number_of_stations; i++){
-
-        if (station.id == user_input_station) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }
-}
 
 
 void output_result(int train_sum_of_time, float train_co2_emitted, float train_sum_of_price,
--- src/UI.h
+++ src/UI.h
@@ -4,9 +4,9 @@
 
 // char * input_from_user();
 
-char* start_station_from_user (Station* station);
+Station* start_station_from_user (Station* station);
 
-char* end_station_from_user (Station* station);
+Station* end_station_from_user (Station* station);
 
 int validate_input(int user_input_station, Station station);
 

03353821ea336ca0d633978c40c6bc2d51d421d2
vjohns22 <vjohns22@student.aau.dk>
2022-12-07 14:14:37 +0100

    Fixing calculate.c functions, testing needed. Updated CMakeLists.txt to include the file.

--- src/calculate.c
+++ src/calculate.c
@@ -1,139 +1,80 @@
 #include "calculate.h"
-#include "station.h"
+#include "station.c" //Probably need to do some CMake stuff if we don't want to include the .c files.
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
-// TODO: Pick a solution based on what the input will look like.
+#define AIR_CO2_PER_KM (float)3.14
+#define RAIL_CO2_PER_KM (float)0.14
 
-// Ripped from station.c adjusted to use connection-structs.
-
-size_t connection_list_length(Connection* connection_list){
+/**
+ * Find the number of connections a station has.
+ * @param connection_list The array of connections you wish to count.
+ * @return Counting the length of the array and returning the length.
+ */
+size_t connection_list_length(Connection* connection_list) {
     if(connection_list == NULL){
         return 0;
     }
 
     size_t i = 0;
-    while(connection_list[i].station[0].name != NULL){
+    while(connection_list[i].station->name != NULL){
         i++;
-    } // TODO: Ensure functionality. And probably move/delete.
-
-    return i;
-}
-
-// Ripped from station.c adjusted to use route-structs.
-
-size_t route_list_length(Route* route_list){
-    if(route_list == NULL){
-        return 0;
     }
 
-    size_t i = 0;
-    while(route_list[i].node1[0].name != NULL){
-        i++;
-    } // TODO: Ensure functionality. And probably move/delete.
-
     return i;
 }
 
-/*
- * If the station-array includes all the stations used in the optimal route,
- * but these station-structs have not been altered, i.e.
- * they still have all their connections and routes.
- *
- * Assuming route.line follows the current Example.json's structure of connection "E-I": "routeID": "EI",
- * meaning route.ID is actually a char* not an int, then:
+/**
+ * Takes the sum of all the prices on the route which has been selected as the optimal route.
+ * @param stations The array of stations that contain the optimal route.
+ * @return The function returns the price of the journey.
  */
-
-float calculate_price(Station* station) {
-    if(station == NULL){
-        return 0;
-    }
+float calculate_price(Station* stations) {
 
     float sum_of_prices = 0;
-    size_t arr_len = station_list_length(station);
+    size_t arr_len = station_list_length(stations);
 
     for (size_t i = 0; i < arr_len; ++i) {
-        char *desired_id = calloc(3, sizeof(char));
-        strcat(desired_id, station[i].id); // Decide which it is; type INT or CHAR (Probably CHAR).
-        strcat(desired_id, station[i + 1].id);
-        desired_id[2] = '\0';
-
-        size_t con_len = connection_list_length(station[i].connections);
-        for (int j = 0; j < con_len; ++j) {
-            size_t route_len = route_list_length(station[i].connections[j].route);
-            for (int k = 0; k < route_len; ++k) {
-                if (strcmp(station[i].connections[j].route[k].line, desired_id)) { //strcomp -- assuming it's char.
-                    sum_of_prices += station[i].connections[j].route[k].price;
-                }
+        size_t connections = connection_list_length(stations[i].connections);
+        for (size_t j = 0; j < connections; ++j) {
+            if (stations[i].connections[j].station->id == stations[i + 1].id) {
+                sum_of_prices += stations[i].connections[j].route->price;
             }
         }
-    }
-    return sum_of_prices;
-}
-
-float calculate_co2(Station* station){
-    if(station == NULL){
-        return 0;
-    }
-
-    int dist_sum = 0;
-    size_t arr_len = station_list_length(station);
-
-    for (size_t i = 0; i < arr_len; ++i) {
-        char *desired_id = calloc(3, sizeof(char));
-        strcat(desired_id, station[i].id); // Decide which it is; type INT or CHAR.
-        strcat(desired_id, station[i + 1].id);
-        desired_id[2] = '\0';
-
-        size_t con_len = connection_list_length(station[i].connections);
-        for (int j = 0; j < con_len; ++j) {
-            size_t route_len = route_list_length(station[i].connections[j].route);
-            for (int k = 0; k < route_len; ++k) {
-                if (strcmp(station[i].connections[j].route[k].line, desired_id)) {
-                    dist_sum += station[i].connections[j].route[k].distance;
-                }
-            }
-        }
-    }
-    float co2_emitted = dist_sum * CO2_PER_KM;
-    //TODO: Define a symbolic constant for CO2 per km.
-    return co2_emitted;
-}
-
-
-/*
- * If the given station-array only has the stations on the optimal route
- * and the stations only contain the used connection and the used route:
-
-float calculate_price(Station* station) {
-    if(station == NULL){
-        return 0;
-    }
-
-    float sum_of_prices = 0;
-    size_t arr_len = station_list_length(station);
-
-    for (size_t i = 0; i < arr_len; ++i) {
-        sum_of_prices += station[i].connections[0].route[0].price;
     }
 
     return sum_of_prices;
 }
 
-float calculate_co2(Station* station){
-    if(station == NULL){
-        return 0;
-    }
-
-    int dist_sum = 0;
-    size_t arr_len = station_list_length(station);
+/**
+ * Takes the distance of each route in the optimal path, then multiplies it by a predefined
+ * CO2/km amount and sums it into one result.
+ * @param stations The array of stations that contain the optimal route.
+ * @return The function returns the estimated CO2 emission of the journey.
+ */
+float calculate_co2(Station* stations){
+    float result = 0;
+    size_t arr_len = station_list_length(stations);
 
     for (size_t i = 0; i < arr_len; ++i) {
-        dist_sum += station[i].connections[0].route[0].distance;
+        size_t connections = connection_list_length(stations[i].connections);
+        for (size_t j = 0; j < connections; ++j) {
+            if (stations[i].connections[j].station->id == stations[i+1].id) {
+                switch(stations[i].connections[j].route->type) {
+                    case AIR:
+                        result += (float) (stations[i].connections[j].route->distance) * AIR_CO2_PER_KM;
+                        break;
+                    case RAIL:
+                        result += (float) (stations[i].connections[j].route->distance) * RAIL_CO2_PER_KM;
+                        break;
+                    default:
+                        printf("TravelType is neither AIR nor RAIL.");
+                        exit(EXIT_FAILURE);
+                }
+            }
+        }
     }
 
-    float co2_emitted = dist_sum * CO2_PER_KM;
-    //TODO: Define a symbolic constant for CO2 per km.
-
-    return co2_emitted;
-} */
+    //Set the actual value of AIR_CO2_PER_KM and RAIL_CO2_PER_KM to the desired values.
+    return result;
+}

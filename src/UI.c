2a438aa8671d217e49f6964566097f7f21e43e77
AndreasBM1 <Ak42an@student.aau.dk>
2022-12-08 10:00:42 +0100

    Revert "Made journey start time, deleted unnecessary code in UI.c"
    This reverts commit 5bb02186fdd1ba21f0966d807496c95fc03f96b6.

--- src/UI.c
+++ src/UI.c
@@ -1,9 +1,45 @@
+/*
+ * TODO: Reset smartly after invalid input from user
+ * TODO: Finish validate input
+ * TODO: How should i output the two stations?
+ */
 
 #include "UI.h"
 #include "station.h"
 #include <stdio.h>
+#include <malloc.h>
 #include <stdbool.h>
+/*
+char* input_from_user ()
+{
+    char user_input_start_station [100],
+         user_input_end_station [100];
 
+    printf("Where does your journey start from\n");
+    scanf("%s ", user_input_start_station);
+
+
+    // check if the station is valid. If invalid ask for first station again
+    if (validate_input(user_input_start_station, Station station) == 1) {
+        printf("Where would you like to go?\n");
+        scanf("%s ", user_input_end_station);
+
+
+        // Check if destination station is valid. If invalid ask for destination station again
+        if (validate_input(user_input_end_station, Station station) == 0){
+            printf("invalid input\n");
+
+            // do something to get second station again that is not stupid
+        }
+
+    }
+    else {
+        printf("Invalid station\n");
+
+        // do something to get first station again that is not stupid
+    }
+}
+*/
 
 Station* start_station_from_user (Station* station){
     char user_input_start_station;
@@ -37,12 +73,21 @@ Station* end_station_from_user (Station* station){
     return get_station_by_id(station, user_input_end_station);
 }
 
-Station* journey_start_time_from_user(Station* station){
-    int hours = 0, minutes = 0;
-    printf("At what time would you like to start your journey? \n"
-           "(Please write in HH:MM format \n)");
-    scanf("%d:%d",&hours, &minutes);
 
+// Function for checking whether the input station is valid or invalid
+int validate_input(int user_input_station, Station station){
+
+    int number_of_stations = sizeof(station.id);
+
+    for (int i = 0; i < number_of_stations; i++){
+
+        if (station.id == user_input_station) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
 }
 
 

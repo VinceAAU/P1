e52eabbc7d9f3f2872c60545b6e311d4c325a33e
Vincent <vbechm22@student.aau.dk>
2022-12-14 09:25:05 +0100

    Added a teensy bit of documentation

--- src/JSON-interface.h
+++ src/JSON-interface.h
@@ -4,4 +4,9 @@
 
 #include "station.h"
 
+/**
+ * Converts JSON data to Station array
+ * @param filename The JSON file's filename
+ * @return A zero-terminated station array.
+ */
 Station* retrieve_JSON_data(char* filename);
